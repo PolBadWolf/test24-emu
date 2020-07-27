d86eadf12da2cf3c510c45c10b5e5641b32dd586
PolBadWolf <gm.gameoff@gmail.com>
2020-07-27 12:32:08 +0500

    add lib Int2Bytes

---
+++ src/main/java/org/example/test24/emu/lib/ConvertDigit.java
@@ -0,0 +1,13 @@
+package org.example.test24.emu.lib;
+
+public class ConvertDigit {
+    public static void Int2bytes(int source, byte[] target, int indx) {
+        int i = 4;
+        do {
+            target[indx] = (byte) (source & 0xff);
+            source >>= 8;
+            indx++;
+            i--;
+        } while (i > 0);
+    }
+}
